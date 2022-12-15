18d6a657d6ec0cbc5dbe03b09e898c6084a45d89
gtgaleevtimur <gtgaleevtimur@gmail.com>
2022-12-15 11:18:02 +0300

    fix

--- internal/handler/getwithdrawals.go
+++ internal/handler/getwithdrawals.go
@@ -34,8 +34,8 @@ func (c *Controller) GetWithdrawals(w http.ResponseWriter, r *http.Request) {
 		c.error(w, r, fmt.Errorf("failed to marshal JSON - %s", err.Error()), http.StatusInternalServerError)
 		return
 	}
-	log.Info().Msg(string(body))
-	w.WriteHeader(http.StatusOK)
+	log.Warn().Msg(string(body))
+	//w.WriteHeader(http.StatusOK)
 	w.Header().Set("Content-Type", ContentTypeApplicationJSON)
 	w.Write(body)
 }
